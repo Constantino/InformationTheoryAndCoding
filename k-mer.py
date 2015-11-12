4c0580b85d4af99a69149796b817744bec8853f6
Constantino <constantinomora@gmail.com>
2015-11-11 20:28:11 -0600

    find most frequent k-mer

--- k-mer.py
+++ k-mer.py
@@ -2,11 +2,12 @@
 from sys import argv, exit
 
 text = argv[1]
-ks = int(argv[2])
 
 len_text = len(text)
 counting = dict()
 
+frequencies = []
+buffer_last_values = dict()
 def  parsing_text(pos,k,text):
     
     sub_string = text[pos:pos+k]
@@ -22,6 +23,9 @@ def  parsing_text(pos,k,text):
 
         count_sub_string(pos, k, sub_string, len_sub_string, text,counting)
 
+        buffer_last_values[sub_string] = counting[sub_string]
+        frequencies.append(counting[sub_string])
+
     return parsing_text(pos+1,k,text)
 
 def count_sub_string(pos,k,sub_string,len_sub_string, text, counting):
@@ -34,11 +38,39 @@ def count_sub_string(pos,k,sub_string,len_sub_string, text, counting):
 
     return count_sub_string(pos+1,k,sub_string, len_sub_string,text,counting)
 
+k_mer = dict()
+k_mer_most_freq = {"e": 0, "value":0}
+def find_kmer_most_frequent():
+    for e in counting:
+        k_mer[e] = len(e)*counting[e]
+        if k_mer[e] > k_mer_most_freq["value"]:
+            k_mer_most_freq["e"] = e
+            k_mer_most_freq["value"] = k_mer[e]
 
-for k in range(1,len_text+1):
-    print "k: ",k
+    print "k_mer_most_freq: ",k_mer_most_freq
+
+still_continue = True
+k = 1
+while (still_continue and k < len_text):
+    
+    frequencies = []
+    buffer_last_values = {}
     parsing_text(0,k,text)
-    print counting
+    
+    still_continue = False
+    for e in frequencies: #Continue while at least one of the frequencies is greater than 1
+        if e > 1:
+            still_continue = True
+
+    if still_continue == False: #if all frequencies == 1 then remove those keys from the dictionary
+        for e in buffer_last_values:
+            del counting[e]
+
+    k += 1
+    
+#print "dict counting: ", counting
+
+find_kmer_most_frequent()
 
 
-print "dict counting: ", counting
+
