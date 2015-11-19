acddb62d50a5440cad65b09c20b6efab8c10ea95
VictorRiosM <victor.riosm@live.com>
2015-11-18 22:02:42 -0600

    Arithmetic coding: encode

--- arithmeticCoding.py
+++ arithmeticCoding.py
@@ -3,36 +3,37 @@ from sys import argv
 from collections import Counter, OrderedDict
 
 def compute_ranges(probabilities):
-	keys = probabilities.keys()
-	ranges = dict()
-	ranges[keys[0]] = (0.0, probabilities[keys[0]])
-	accumulated = probabilities[keys[0]]
-	for i in range(len(keys))[1:-1]:
-		ranges[keys[i]] = (accumulated, accumulated+probabilities[keys[i]])
-		accumulated += probabilities[keys[i]]
-	ranges[keys[-1]] = (accumulated, 1.0)
-	print ranges
-	return ranges	
+    keys = probabilities.keys()
+    ranges = dict()
+    ranges[keys[0]] = (0.0, probabilities[keys[0]])
+    accumulated = probabilities[keys[0]]
+    for i in range(len(keys))[1:-1]:
+        ranges[keys[i]] = (accumulated, accumulated+probabilities[keys[i]])
+        accumulated += probabilities[keys[i]]
+    ranges[keys[-1]] = (accumulated, 1.0)
+    print "Ranges", ranges
+    return ranges   
 
 def arithmeticCoding(probabilities, word):
-	encoded = ""
-	high = 1.0
-	low = 1.0
-	ranges = compute_ranges(probabilities)
-	#for s in word:
-		#a = 
-		#subinterval(a, b)
-	return encoded
+    high = 1.0
+    low = 0.0
+    ranges = compute_ranges(probabilities)
+    for symbol in word:
+        rang = high - low
+        high = low + rang*ranges[symbol][1]
+        low = low + rang*ranges[symbol][0]
+    return (high+low)/2
 
 if __name__=='__main__':
-	try:
-		string = argv[1]
-	except:
-		#string = "no llores la muerte de tu cuerpo ni llores la muerte de tu alma."
-		string = "alaladoaroloro"
-	frequencies = Counter(string)
-	probability = {s : float(frequencies[s]) / len(string) for s in frequencies}
-	print probability
-	word = 'ala' # Word to be encoded
-	encoded = arithmeticCoding(probability, word)
+    try:
+        string = argv[1]
+    except:
+        #string = "no llores la muerte de tu cuerpo ni llores la muerte de tu alma."
+        string = "alaladoaroloro" # String for testing purposes
 
+    frequencies = Counter(string)
+    probability = {s : float(frequencies[s]) / len(string) for s in frequencies}
+
+    word = 'al' # Word to be encoded for testing
+    encoded = arithmeticCoding(probability, word)
+    print word + " =", encoded
