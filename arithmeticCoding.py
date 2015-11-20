d52ff14e7c055122a89fa8d38668d24fee30b8f2
Constantino Mora <constantinomora@gmail.com>
2015-11-20 03:01:41 -0600

    Method 1: Finding the dfwld in a subinterval

--- arithmeticCoding.py
+++ arithmeticCoding.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2
 from sys import argv
 from collections import Counter, OrderedDict
+from math import pow,ceil
 
 def compute_ranges(probabilities):
     keys = probabilities.keys()
@@ -18,11 +19,51 @@ def arithmeticCoding(probabilities, word):
     high = 1.0
     low = 0.0
     ranges = compute_ranges(probabilities)
+
+    #Testing
+    t = find_t(.0612)
+    x = find_x(0.1020,0.1632,t)
+    r = find_r(x,t)
+    
+    print "t: ",t
+    print "x: ",x
+    print "r: ",r
+    
+    return r
+    
+    """
     for symbol in word:
         rang = high - low
         high = low + rang*ranges[symbol][1]
         low = low + rang*ranges[symbol][0]
     return (high+low)/2
+    """
+
+#Method 1
+def find_t(l):
+    t = 0
+    while (1.0/pow(2,t) > l):
+        t += 1
+
+    return t
+
+def find_x(alfa, beta, t):
+    den = pow(2,t)
+    alfa *= den
+    beta *= den
+
+    integers = []
+
+    for e in range(int(ceil(alfa)),int(ceil(beta))):
+        integers.append(e)
+
+    for e in integers:
+        if e%2 == 0:
+            return e
+    return 
+
+def find_r(x,t):
+    return x/pow(2,t)
 
 def binary_expansion(word, binary):
     word = word*2
@@ -39,8 +80,8 @@ if __name__=='__main__':
     try:
         string = argv[1]
     except:
-        string = "no llores la muerte de tu cuerpo ni llores la muerte de tu alma."
-        #string = "alaladoaroloro" # String for testing purposes
+        #string = "no llores la muerte de tu cuerpo ni llores la muerte de tu alma."
+        string = "alaladoaroloro" # String for testing purposes
 
     frequencies = Counter(string)
     for f in frequencies:
